de42937d4c78d709bf78b977ac961e9ce89eadd9
Mikołaj Przenzak <mprzenzak@it-dev.pl>
2022-10-19 13:54:59 +0200

    init

---
+++ WebCamController/WebCamController/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace WebCamController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var projectController = new ProjectController();
+            projectController.TakePhoto();
+        }
+    }
+}
---
+++ WebCamController/WebCamController/ProjectController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Media.Capture;
+using Windows.Storage;
+
+namespace WebCamController
+{
+    public class ProjectController
+    {
+        public async void TakePhoto()
+        {
+            CameraCaptureUI dialog = new CameraCaptureUI();
+            Size aspectRatio = new Size(16, 9);
+            dialog.PhotoSettings.CroppedAspectRatio = aspectRatio;
+
+            StorageFile file = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile sampleFile = await storageFolder.CreateFileAsync("photo.png",
+            CreationCollisionOption.ReplaceExisting);
+        }
+
+    }
+}
